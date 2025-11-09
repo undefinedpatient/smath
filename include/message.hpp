0f63949ba128a72114c0cfbe44bafdb77020b7ff
undefinedpatient <undefinedpatient@gmail.com>
2025-11-09 17:55:47 +0800

    Updated complete basic Vec3 class

--- include/message.hpp
+++ include/message.hpp
@@ -1,7 +1,3 @@
-//
-// Created by undef on 03/11/2025.
-//
-
 #ifndef SMATH_MESSAGE_H
 #define SMATH_MESSAGE_H
 #include <iostream>
@@ -9,8 +5,10 @@
 constexpr int MESSAGE_WIDTH = 64;
 inline void print_message(const char& deco, const std::string &message) {
     const int pad = (MESSAGE_WIDTH - static_cast<int>(message.length()))/2;
+    std::
     std::cout << "+" << std::string(pad, deco) << " ";
     std::cout << message << " ";
     std::cout << std::string(pad, deco) << "+" << std::endl;
 }
+
 #endif //SMATH_MESSAGE_H
